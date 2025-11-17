a108c940521f3d9d885e838dff7ce5949a3f0eff
Akhila0606 <akhilapothu49@gmail.com>
2025-11-17 20:49:24 +0530

    first commit

---
+++ app1/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from app1.models import employees
+
+class employee_admin(admin.ModelAdmin):
+    list_display=['emp_name','emp_id','emp_email']
+admin.site.register(employees,employee_admin)
